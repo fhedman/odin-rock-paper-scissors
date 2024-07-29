9af01a20803d43836cd4b2e7607e191f5f85103e
plantbrew <76662039+plantbrew@users.noreply.github.com>
2024-07-29 10:05:22 +0200

    Polish design

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-playGame();
+let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 100);
-    
+
     if (choice < 33){
         return "rock";
     }
@@ -14,53 +15,114 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Choose rock, paper or scissors");
-    return choice.toLowerCase();
+let buttons = document.querySelector('#score');
+
+buttons.addEventListener('click', function(event) {
+    let target = event.target;
+
+    switch(target.id) {
+        case 'rock':
+            console.log('Rock was clicked');
+            playRound('rock', getComputerChoice());
+            break;
+        case 'paper':
+            console.log('Paper was clicked');
+            playRound('paper', getComputerChoice());
+            break;
+        case 'scissors':
+            console.log('Scissors was clicked');
+            playRound('scissors', getComputerChoice());
+            break;
+        }
+});
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        console.log("It's a tie!");
+        colorButton('tie', computerChoice, playerChoice);
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats scissors");
+        colorButton('player', computerChoice, playerChoice);
+        playerScore++;
+        setScore('player', playerScore + '');
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats rock");
+        colorButton('computer', computerChoice, playerChoice);
+        computerScore++;
+        setScore('computer', computerScore + '');
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats rock");
+        colorButton('player', computerChoice, playerChoice);
+        playerScore++;
+        setScore('player', playerScore + '');
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats paper");
+        colorButton('computer', computerChoice, playerChoice);
+        computerScore++;
+        setScore('computer', computerScore + '');
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats paper");
+        colorButton('player', computerChoice, playerChoice);
+        playerScore++;
+        setScore('player', playerScore + '');
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats scissors");
+        colorButton('computer', computerChoice, playerChoice);
+        computerScore++;
+        setScore('computer', computerScore + '');
+    }
+
+    setTimeout(resetButtons, 300);
+
+    if (playerScore === 5 || computerScore === 5) {
+        let finalMessage = "";
+        if (playerScore > computerScore) {
+            finalMessage = "FINAL RESULT: You win " + playerScore + "-" + computerScore + " with " + playerChoice + "!";
+        } else if (computerScore > playerScore) {
+            finalMessage = "FINAL RESULT: Computer wins " + computerScore + "-" + playerScore + " with " + computerChoice + "!";
+        } else {
+            finalMessage = "FINAL RESULT: It's a tie!";
+        }
+        alert(finalMessage);
+        resetGame();
+    }
 }
 
-function playGame(){
 
-    let computerScore = 0;
-    let humanScore = 0;
+function colorButton(winner, loser, choice) {
+    let playerButton = document.querySelector('#' + choice);
+    let computerButton = document.querySelector('#c' + loser);
 
-    for (let i = 1; i <= 5; i++) {
-        console.log("Round: " + i)
-        let computerSelection = getComputerChoice();
-        let humanSelection = getHumanChoice();
-        playRound(humanSelection, computerSelection);
-        console.log("SCORE: Human: " + humanScore + " Computer : " + computerScore);
-    }
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log("It's a tie!");
-        } else if (humanChoice === "rock" && computerChoice === "scissors"){
-            console.log("You win! Rock beats scissors");
-            humanScore++;
-        } else if (humanChoice === "rock" && computerChoice === "paper"){
-            console.log("You lose! Paper beats rock");
-            computerScore++;
-        } else if (humanChoice === "paper" && computerChoice === "rock"){
-            console.log("You win! Paper beats rock");
-            humanScore++;
-        } else if (humanChoice === "paper" && computerChoice === "scissors"){
-            console.log("You lose! Scissors beats paper");
-            computerScore++;
-        } else if (humanChoice === "scissors" && computerChoice === "paper"){
-            console.log("You win! Scissors beats paper");
-            humanScore++;
-        } else if (humanChoice === "scissors" && computerChoice === "rock"){
-            console.log("You lose! Rock beats scissors");
-            computerScore++;
-        }
-    }
-
-    if (computerScore === humanScore) {
-        console.log("FINAL RESULT: Tie!");
-    } else if (humanScore > computerScore) {
-        console.log("FINAL RESULT: Human wins!");
+    if (winner === 'player') {
+        playerButton.style.background='#2cb02c';
+        computerButton.style.background= '#ff4b42';
+    } else if (winner === 'computer'){
+        playerButton.style.background='#ff4b42';
+        computerButton.style.background= '#2cb02c';
     } else {
-        console.log("FINAL RESULT: Computer wins!");
+        playerButton.style.background='#ff4b42';
+        computerButton.style.background= '#ff4b42';
     }
+}
+
+function resetButtons () {
+    document.querySelectorAll('.button').forEach(button => {
+        button.style.background = ''; 
+    });
+}
+
+function setScore (winner, score){
+    document.querySelector('#' + winner + 'Score').textContent = score;
+}
+
+function resetScore () {
+    document.querySelector('#playerScore').textContent = 0;
+    document.querySelector('#computerScore').textContent = 0;
+}
+
+function resetGame () {
+    playerScore = 0;
+    computerScore = 0;
+    resetScore();
+    resetButtons();
 }
